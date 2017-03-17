74c1dbe9b0945125a1ae41a798012cdd58072495
mm <mm@aules-103-248.udl.net>
2017-03-17 11:00:54 +0100

    Solver + max_tries + flips

--- solver.py
+++ solver.py
@@ -38,14 +38,20 @@ class Problem:
                 self.num_clauses = int(line[3])
         self.clauses = clauses
         #print clauses
-        solution = self.solve()
-        self.print_solution(solution)
+        self.solve()
+        self.print_nosolution()
 
     def solve(self):
+        max_tries = 100
+        max_flips = 100
+        for i in xrange(0, max_tries):
+            inter = self.generate_interpretation()
+            for j in xrange(0, max_flips):
+                var, cl = self.is_satisfiable(inter)
+                inter = self.flip_inter(inter, cl)
+        return False
 
-        inter = self.generate_interpretation()
-        print inter
-        print self.clauses
+    def is_satisfiable(self, inter):
         for cl in self.clauses:
             sat = False
             for x in cl:
@@ -53,27 +59,36 @@ class Problem:
                     sat = True
                     break
             if not sat:
-                return False
+                return x, cl
 
         self.inter = inter
-        return True
+        self.print_solution()
+
+    def flip_inter(self, inter, cl):
+        lit = cl[random.randint(0, len(cl)-1)]
+        inter[abs(lit)-1] = inter[abs(lit)-1] * -1
+        return inter
+
 
     def generate_interpretation(self):
         inter = list(xrange(1, self.num_vars+1))
+        for x in xrange(0, len(inter)-1):
+            if random.random() > 0.5:
+                inter[x] = inter[x] * -1
         return inter
 
-    def print_solution(self, sol):
-        if sol:
-            print "s SATISFIABLE"
-            print "v",
-            for item in self.inter:
-                print item,
-        else:
-            print "s UNSATISFIABLE"
+    def print_solution(self):
+        print "s SATISFIABLE"
+        print "v",
+        for item in self.inter:
+            print item,
+        sys.exit(0)
 
-
+    def print_nosolution(self):
+        print "s UNSATISFIABLE"
 
 if __name__ == '__main__':
+    random.seed()
     solver = Problem()
     f = solver.read_file(sys.argv[1])
     solver.read_clauses(sys.argv[1])
