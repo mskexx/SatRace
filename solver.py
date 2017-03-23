ac6d0c0d1a283dce64b096ac080ba0e52ce01f03
mm <mm@aules-103-022.udl.net>
2017-03-23 10:14:48 +0100

    Added Docstrings

--- solver.py
+++ solver.py
@@ -3,7 +3,8 @@ import sys
 import random
 
 
-class Problem:
+class Problem(object):
+    ''' Read input problem -> [generate interpretation <> find Solution <]'''
 
     def __init__(self):
         self.num_vars = 0
@@ -12,6 +13,7 @@ class Problem:
         self.inter = []
 
     def read_clauses(self, cnf_file):
+        ''' Read input and save clauses in a list of lists '''
         clause = []
         clauses = []
 
@@ -19,9 +21,9 @@ class Problem:
         for line in data:
             line = line.split()
             if line[0] not in ('c', 'p'):
-                for x in line:
-                    if x != '0':
-                        clause.append(int(x))
+                for i in line:
+                    if i != '0':
+                        clause.append(int(i))
                 clauses.append(clause)
                 clause = []
 
@@ -32,6 +34,7 @@ class Problem:
         self.solve()
 
     def solve(self):
+        '''Check if all clauses are satisfied with diferents interpretations'''
         max_tries = 9999999
         max_flips = 200000
         for i in xrange(0, max_tries):
@@ -42,24 +45,27 @@ class Problem:
         self.print_nosolution()
 
     def is_satisfiable(self, inter):
-        for cl in self.clauses:
+        ''' Check if the given clause is satisfied '''
+        for clause in self.clauses:
             sat = False
-            for x in cl:
-                if x == inter[abs(x) - 1]:
+            for i in clause:
+                if i == inter[abs(i) - 1]:
                     sat = True
                     break
             if not sat:
-                return cl
+                return clause
 
         self.inter = inter
         self.print_solution()
 
     def flip_inter(self, inter, sat):
+        ''' Change a variable randomly'''
         lit = sat[random.randint(0, len(sat) - 1)]
         inter[abs(lit) - 1] = inter[abs(lit) - 1] * -1
         return inter
 
     def generate_interpretation(self):
+        ''' Generate a random interpretation '''
         inter = list(xrange(1, self.num_vars + 1))
         for i in xrange(0, len(inter) - 1):
             if random.random() > 0.5:
@@ -67,18 +73,22 @@ class Problem:
         return inter
 
     def print_solution(self):
+        ''' s SATISFIABLE
+            v 1 2 3 -4 -5 -6 7 8 9 -10
+        '''
         print "s SATISFIABLE"
         print "v",
         for item in self.inter:
             print item,
         sys.exit(0)
 
-    def print_nosolution(self):
+    @staticmethod
+    def print_nosolution():
+        ''' Print no solution '''
         print "s No Solution Found"
         sys.exit(0)
 
 
 if __name__ == '__main__':
     random.seed()
-    solver = Problem()
-    solver.read_clauses(sys.argv[1])
+    Problem().read_clauses(sys.argv[1])
